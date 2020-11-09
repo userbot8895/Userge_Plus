976e5a6756b8d7543c31eb9a582d8bec4bfab8bc
Narek Torosyan <narek.email@gmail.com>
2020-11-09 23:56:07 +0400

    Create flasher.py

---
+++ userge/plugins/plus_beta/flasher.py
@@ -0,0 +1,27 @@
+# flasher
+# Userge++ (beta)
+# Licensed under the DBBPL
+# (C) 2020 githubcatw
+
+import time
+import random
+
+from userge import userge, Message, filters
+
+LOG = userge.getLogger(__name__)  # logger object
+CHANNEL = userge.getCLogger(__name__)  # channel logger object
+
+# add command handler
+@userge.on_cmd("flash", about="flashes")
+async def flash(message: Message):
+	r = random.randint(1, 10000)
+	text = message.text
+	if len(text.split(" ")) > 1:
+		await message.edit("`Cannot flash file!`")
+		return
+	await message.edit(f"`Flashing` {text}.zip`...`")
+	time.sleep(4)
+	if r % 2 == 1:
+		await message.edit(f"`Successfully flashed` {text}.zip`!`")
+	elif r % 2 == 0:
+		await message.edit(f"`Flashing` {text}.zip `failed successfully!`")
